8a2d65f3744c4ac7ce39bc7abb5aa4783bbd6bb2
kathecr <kcastelblancor@gmail.com>
2021-04-21 22:38:42 -0500

    Create template Home

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./scr/index.js",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
@@ -22,7 +22,7 @@ module.exports = {
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin([
+    new HtmlWebPackPlugin([
       {
         inject: true,
         template: "./public/index.html",
